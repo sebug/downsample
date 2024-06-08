7f91b39eab98d827a92df0dcf8652f45b4feba5b
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-06-08 22:44:12 +0200

    to grayscale

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -6,11 +6,37 @@ const canvas = window.canvas = document.querySelector('canvas');
 canvas.width = 480;
 canvas.height = 360;
 
+function RGBToGrayScale(red,green,blue) {
+    //return red * 0.2126 + green * 0.7152 + blue * 0.0722;
+    return (red * 6966 + green * 23436 + blue * 2366) >> 15;
+}  
+
 const button = document.querySelector('button');
 button.onclick = function() {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
-  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+  let transformCanvas = document.createElement('canvas');
+  transformCanvas.width = 160;
+  transformCanvas.height = 120;
+  let transformCtx = transformCanvas.getContext('2d');
+  transformCtx.drawImage(video, 0, 0, transformCanvas.width, transformCanvas.height);
+
+  let imageData = transformCtx.getImageData(0, 0, transformCanvas.width, transformCanvas.height);
+  const pixels = imageData.data;
+  for (var i = 0; i < pixels.length; i += 4) {
+
+    let lightness = parseInt((pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3);
+
+    pixels[i] = lightness;
+    pixels[i + 1] = lightness;
+    pixels[i + 2] = lightness;
+  }
+  transformCtx.putImageData(imageData, 0, 0);
+
+  let ctx = canvas.getContext('2d');
+  ctx.imageSmoothingEnabled = false;
+
+  ctx.drawImage(transformCanvas, 0, 0, canvas.width, canvas.height);
 };
 
 const constraints = {
